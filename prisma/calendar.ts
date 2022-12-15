9550dcfdce43da7e2274b74be6c3c172991dd241
aashish2057 <aashish2057@gmail.com>
2022-12-15 17:37:02 -0600

    fixed getDate function to also get the tasklist stored in a given day

--- prisma/calendar.ts
+++ prisma/calendar.ts
@@ -3,7 +3,7 @@ import prisma from '.';
 
 export async function getDate(date: Date) {
     try {
-        const day = await prisma.calendar.findUnique({ where: { date }})
+        const day = await prisma.calendar.findUnique({ where: { date }, include: {taskList: true}})
         return { day }
     } catch (error) {
         return {error}
