773c7b5708b673120a53152218d5246ba5b59f23
aashish2057 <aashish2057@gmail.com>
2022-12-22 16:51:03 -0600

    fix layout of days

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,35 +1,74 @@
 import Link from "next/link";
 import { data } from "../data"
 
+interface Day {
+    day: {
+        date: Date;
+        completionPercentage: number;
+        taskList: {
+            task: string;
+            isDone: boolean;
+        }[];
+    }
+}
+
+function getDay(date: Date) {
+    const weekNum = date.getDay()
+    let weekDay;
+    if (weekNum === 0) {
+        weekDay = "Sunday"
+    }
+    if (weekNum === 1) {
+        weekDay = "Monday"
+    }
+    if (weekNum === 2) {
+        weekDay = "Tuesday"
+    }
+    if (weekNum === 3) {
+       weekDay = "Wensday"
+    }
+    if (weekNum === 4) {
+        weekDay = "Thursday"
+    }
+    if (weekNum === 5) {
+        weekDay = "Friday"
+    }
+    if (weekNum === 6) {
+        weekDay = "Saturday"
+    }
+    return weekDay;
+}
+
+
 export default function TaskPage() {
     return (
         <div>
             <h1>Tasks</h1>
-            <div>
-                {data?.map((day) => {
-                    return <Day key={day.date} day={day} />;
-                })}
+            <div className="flex flex-row gap-6">
+                {data.map((day) => 
+                    <div className="p-10">
+                        <Day day={day} />
+                    </div> 
+                )}
             </div>
         </div>
     )
 }
 
-function Day({ day }: any) {
+function Day({ day }: Day) {
     const {date, completionPercentage, taskList} = day || {};
     console.log(taskList)
     return (
-        <Link href={`/tasks/${date}`}>
+        <div>
+            <h2>{getDay(date)}</h2>
             <div>
-                <h2>{date}</h2>
-                <p>{completionPercentage}</p>
-                <div>
-                    <li>
-                        {taskList?.map((t) => {
-                            return <div>{t.task}</div>
-                        })}
-                    </li>
-                </div>
+                {taskList.map((t) => 
+                    <div className="flex flex-row gap-2">
+                        <input type="checkbox"/> <ul>{t.task}</ul> <p>{String(t.isDone)}</p>
+                    </div>
+                )}
             </div>
-        </Link>
+        </div>
     )
 }
+
